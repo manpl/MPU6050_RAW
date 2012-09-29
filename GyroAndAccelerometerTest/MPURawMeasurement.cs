c25570cc9e08b6268bc0fd30a3ab4094dfc73578
manpl <nawrockim@op.pl>
2012-09-29 14:44:30 +0200

    MPU6050 Cleanup

--- GyroAndAccelerometerTest/MPURawMeasurement.cs
+++ GyroAndAccelerometerTest/MPURawMeasurement.cs
@@ -14,14 +14,14 @@ namespace GyroAndAccelerometerTest
     {
         private Measurement InitialRawGyro = new Measurement();
         private Measurement LatestGyro = new Measurement();
-
-//        private Measurement InitialRawAcc = new Measurement();
         private Measurement LatestAcc = new Measurement();
+
+        private const double gyroScale = 65.6;
+        private const double time = 0.001;
+        private const double complementFactor = 0.9;
 
         private double xAngle;
         private double yAngle;
-
-//        private double Temperature { get; set; }
 
         public MPU6050(ArrayList rawMeasurements)
         {
@@ -30,18 +30,10 @@ namespace GyroAndAccelerometerTest
 
             foreach (byte[] bits in rawMeasurements)
             {
-                //InitialRawAcc.X +=  bits.TwoBytesToInt( 0, 1);
-                //InitialRawAcc.Y +=  bits.TwoBytesToInt( 2, 3);
-                //InitialRawAcc.Z +=  bits.TwoBytesToInt( 4, 5);
-                
                 InitialRawGyro.X += bits.TwoBytesToInt( 8, 9);
                 InitialRawGyro.Y += bits.TwoBytesToInt( 10, 11);
                 InitialRawGyro.Z += bits.TwoBytesToInt( 12, 13);
             }
-
-            //InitialRawAcc.X /= rawMeasurements.Count;
-            //InitialRawAcc.Y /= rawMeasurements.Count;
-            //InitialRawAcc.Z /= rawMeasurements.Count;
 
             InitialRawGyro.X /= rawMeasurements.Count;
             InitialRawGyro.Y /= rawMeasurements.Count;
@@ -51,10 +43,6 @@ namespace GyroAndAccelerometerTest
         public void Update(byte[] rawMeasurement)
         {
             UpdateAccelerometer(rawMeasurement);
-
-            //double rawTempReading = rawMeasurement.TwoBytesToInt(6, 7);
-            //Temperature = ((double)rawTempReading + 12412.0) / 340.0;
-
             UpdateGyro(rawMeasurement);
         }
 
@@ -71,12 +59,9 @@ namespace GyroAndAccelerometerTest
             double gyroY = rawMeasurement.TwoBytesToInt(10, 11);
             double gyroZ = rawMeasurement.TwoBytesToInt(12, 13);
 
-            var scale = 65.5;
-            var time = 0.001;
-
-            LatestGyro.X = (gyroX - InitialRawGyro.X) / scale * time;
-            LatestGyro.Y = (gyroY - InitialRawGyro.Y) / scale * time;
-            LatestGyro.Z = (gyroZ - InitialRawGyro.Z) / scale * time;
+            LatestGyro.X = (gyroX - InitialRawGyro.X) / gyroScale * time;
+            LatestGyro.Y = (gyroY - InitialRawGyro.Y) / gyroScale * time;
+            LatestGyro.Z = (gyroZ - InitialRawGyro.Z) / gyroScale * time;
         }
 
         public void ComputeAngles()
@@ -85,9 +70,8 @@ namespace GyroAndAccelerometerTest
             double accXAngle = 57.295 * Trigo.Atan((float)this.LatestAcc.Y / Trigo.Sqrt(Trigo.Pow((float)this.LatestAcc.Z, 2) + Trigo.Pow((float)LatestAcc.X, 2)));
             double accYAngle = 57.295 * Trigo.Atan((float)-LatestAcc.X / Trigo.Sqrt(Trigo.Pow((float)LatestAcc.Z, 2) + Trigo.Pow((float)LatestAcc.Y, 2)));
 
-            var factor = 0.9;
-            xAngle = xAngle * LatestGyro.X * (1-factor) + accXAngle * factor;
-            yAngle = yAngle * LatestGyro.Y * (1-factor) + accYAngle * factor;
+            xAngle = xAngle * LatestGyro.X * (1 - complementFactor) + accXAngle * complementFactor;
+            yAngle = yAngle * LatestGyro.Y * (1 - complementFactor) + accYAngle * complementFactor;
         }
 
         public void PrintAngles()
